be5873a9282cda1342706b902f4f0475aaf01065
mcnuggetsx20 <alienbaxy@gmail.com>
2023-12-12 12:56:46 +0100

    12 Dec 2023 12:56:46

--- .config/nvim/lua/core/functions.lua
+++ .config/nvim/lua/core/functions.lua
@@ -1,9 +1,5 @@
-function ExecuteInShell(command)
-  local output = vim.fn.systemlist(command)
-
-  local original_bufnr = vim.fn.bufnr('%')
-
-  -- Sprawdź, czy bufor o nazwie "output" już istnieje
+function ExecuteToWindow(command)
+  --sprawdzamy czy taki bufor juz istnieje zeby go w razie w wyjebac
   local existing_bufnr = vim.fn.bufnr("output")
   if existing_bufnr ~= -1 and vim.fn.bufexists(existing_bufnr) == 1 then
     -- Bufor już istnieje, przełącz się do niego
@@ -11,27 +7,25 @@ function ExecuteInShell(command)
     print('Buffer "output" already exists. Shell command ' .. command .. ' executed.')
   end
 
-  --tworzenie nowego bufora
-  local bufnr = vim.api.nvim_create_buf(false, true)
-  vim.api.nvim_set_current_buf(bufnr)
+  local output = vim.fn.systemlist(command) --output komendy
+  vim.cmd('botright 50 vnew') --tworzenie okna
 
-  --opcje tego nowego bufora
-  vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, output) --tutaj wypelniamy go outputem
-  vim.api.nvim_buf_set_option(bufnr, 'buftype', 'nofile')
-  vim.api.nvim_buf_set_option(bufnr, 'bufhidden', 'wipe')
-  vim.api.nvim_buf_set_option(bufnr, 'buflisted', false)
+  local winnr = vim.api.nvim_get_current_win() --po poprzedniej komendzie wjechalismy do nowego okna
+  local bufnr = vim.api.nvim_win_get_buf(winnr) --zdobywanie bufora w tym nowym oknie
+
+  --jakies luzne ustawienia bufora
   vim.api.nvim_buf_set_option(bufnr, 'swapfile', false)
   vim.api.nvim_buf_set_option(bufnr, 'filetype', 'shelloutput')
-  vim.api.nvim_buf_set_option(bufnr, 'readonly', true)
---------------
-  vim.api.nvim_buf_set_name(bufnr, "output") --luzna nazwa bufora
-  vim.cmd('topleft 120 vnew') -- tu sie cos jebi i jak bylo botright to sie odpalalo po lewej wiec dalem topleft i ema; rozmiar jest odwrotnie proporcjonalny
-  vim.cmd('buffer ' .. original_bufnr) --powrot do pierwotengo bufora
+  vim.api.nvim_buf_set_option(bufnr, 'buftype', 'nofile')
+  vim.api.nvim_buf_set_option(bufnr, 'bufhidden', 'wipe')
 
-  print('Shell command ' .. command .. ' executed.')
+  vim.api.nvim_buf_set_name(bufnr, "output") --zmiana nazwy bufora
+  vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, output) --wstawienie wyniku komendy do okna
+
+  vim.cmd('wincmd h')
 end
 
-vim.cmd('command! -complete=shellcmd -nargs=+ Shell call v:lua.ExecuteInShell(<q-args>)')
+vim.cmd('command! -complete=shellcmd -nargs=+ Shell call v:lua.ExecuteToWindow(<q-args>)')
 vim.cmd [[command WW silent! :w !sudo tee %]]
 
 function custom_action()
@@ -45,7 +39,6 @@ function custom_action()
 
   elseif file_type == 'java' then
       vim.cmd("silent Shell javac " .. current_file .. " && java " .. current_file)
-    --nnoremap <F3> :w <bar> Shell javac % && java % <cr>
 
   else
       --default case
